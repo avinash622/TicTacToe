0f3cf4961f3d474a466431abf84b330fdae05fd2
avinash622 <2010030358cse@gmail.com>
2024-11-19 20:57:19 +0530

    TicTacToe.jsx

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -9,60 +9,60 @@ const TicTacToe = () => {
     let [lock,setLock] = useState(false); 
     
     const dis = (e,n) => {
-        if(lock || a[n]!=""){
+        if(lock || a[n]!==""){
             return 0;
         }
         if(c%2 === 0){
             e.target.innerHTML="X";
-            a[n]='x';
+            a[n]='X';
             setC(++c);
         }
         else{
             e.target.innerHTML="O";
-            a[n]='o';
+            a[n]='O';
             setC(++c);
         }
         check();
     }
 
     const check =()=>{
-        if(a[0]===a[1] && a[1] === a[2] && a[2]!=""){
-            won();
+        if(a[0]===a[1] && a[1] === a[2] && a[2]!==""){
+            won(a[0]);
         }
-        if(a[3]===a[4] && a[4] === a[5] && a[5]!=""){
-            won();
-        }if(a[8]===a[7] && a[7] === a[6] && a[6]!=""){
-            won();
-        }if(a[0]===a[3] && a[3] === a[6] && a[6]!=""){
-            won();
-        }if(a[4]===a[1] && a[1] === a[7] && a[7]!=""){
-            won();
+        if(a[3]===a[4] && a[4] === a[5] && a[5]!==""){
+            won(a[3]);
+        }if(a[8]===a[7] && a[7] === a[6] && a[6]!==""){
+            won(a[6]);
+        }if(a[0]===a[3] && a[3] === a[6] && a[6]!==""){
+            won(a[0]);
+        }if(a[4]===a[1] && a[1] === a[7] && a[7]!==""){
+            won(a[1]);
         }
-        if(a[2]===a[5] && a[8] === a[2] && a[2]!=""){
-            won();
+        if(a[2]===a[5] && a[8] === a[2] && a[2]!==""){
+            won(a[2]);
         }
-        if(a[0]===a[4] && a[0] === a[8] && a[0]!=""){
-            won();
+        if(a[0]===a[4] && a[0] === a[8] && a[0]!==""){
+            won(a[0]);
         }
-        if(a[6]===a[4] && a[4] === a[2] && a[2]!=""){
-            won();
+        if(a[6]===a[4] && a[4] === a[2] && a[2]!==""){
+            won(a[2]);
         }
     }
 
-    const won = () => {
+    const won = (y) => {
         setLock(true);
-        x.current.innerHTML="GAME OVER.....!";
+        x.current.innerHTML="GAME OVER.....!<br> PLAYER "+y+" WON";
+
     }
 
     const clear = () => {
         a = ["", "", "", "", "", "", "", "", ""];
         setC(0);
         setLock(false);
-        x.current.innerHTML = "Tic Tac Toe in <span>React</span>"; // Reset the title
+        x.current.innerHTML = "Tic Tac Toe in <span>React</span>"; 
 
-        // Clear the board
         document.querySelectorAll(".box").forEach((box) => {
-            box.innerHTML = ""; // Clear all boxes
+            box.innerHTML = ""; 
         });
     }
     return(
